9e9fb1f9e35caa868ca92193d55fc071df8580ee
kodaimura <kodaimura123@gmail.com>
2023-07-02 15:39:26 +0900

    arrange layout

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,9 +1,19 @@
 document.getElementById('generate').addEventListener('click', () => {
-	document.getElementById('message').innerHTML = ''
+	document.getElementById('message').innerHTML = '';
 
-	const file = document.getElementById('csv').files[0]
+	const file = document.getElementById('csv').files[0];
+	const lang = document.getElementById('lang').value;
+	const rdbms = document.getElementById('rdbms').value;
+
+	if (file === undefined) {
+		renderMessage("csvファイルが選択されていません。", false);
+		return;
+	}
+
 	const formData = new FormData();
 	formData.append('file', file);
+	formData.append('lang', lang);
+	formData.append('rdbms', rdbms);
 
 	fetch('/csv', {
 		method: 'POST',
@@ -28,19 +38,18 @@ const download = (path) => {
 	alink.href = path;
 	alink.click();
 	document.getElementById('csv').value = ''
-	renderMessage(`${path.substring(2).replace('/', '_')} がダウンロードされました。`, true)
+	renderMessage(`${path.substring(2).replace('/', '_')} がダウンロードされました。`, true);
 }
 
 const handleErrors = (errors) => {
-	console.log(errors)
 	for (err of errors) {
-		renderMessage(err, false)
+		renderMessage(err, false);
 	}
 }
 
 const renderMessage = (msg, isSuccess) => {
 	let message = document.createElement('div');
-	message.textContent = msg
+	message.textContent = msg;
 	message.className = `alert alert-${isSuccess? 'success' : 'danger'} alert-custom my-1`;
 	document.getElementById('message').appendChild(message);
 }
