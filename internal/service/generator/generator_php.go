32a3f95104b594f0c2caa2bad663624001c35768
kodaimura <kodaimura123@gmail.com>
2023-07-22 08:51:34 +0900

    generate php templates

--- internal/service/generator/generator_php.go
+++ internal/service/generator/generator_php.go
@@ -1,8 +1,8 @@
 package generator
 
 import (
-	//"os"
-	//"fmt"
+	"os"
+	"fmt"
 	//"strings"
 
 	"masmaint-cg/internal/core/logger"
@@ -38,10 +38,10 @@ func (serv *sourceGeneratorPhp) GenerateSource() error {
 	if err := serv.generateSettingFiles(); err != nil {
 		return err
 	}
-	/*
 	if err := serv.generateTemplates(); err != nil {
 		return err
 	}
+	/*
 	if err := serv.generatePublic(); err != nil {
 		return err
 	}
@@ -106,6 +106,78 @@ func (serv *sourceGeneratorPhp) generateSettingFiles() error {
 	destination = serv.path + "composer.json"
 
 	err = CopyFile(source, destination)
+	if err != nil {
+		logger.LogError(err.Error())
+	}
+	return err
+}
+
+// templates生成
+func (serv *sourceGeneratorPhp) generateTemplates() error {
+	path := serv.path + "templates/"
+
+	if err := os.MkdirAll(path, 0777); err != nil {
+		logger.LogError(err.Error())
+		return err
+	}
+
+	return serv.generateTemplatesFiles(path)
+}
+
+// templates内のファイル生成
+func (serv *sourceGeneratorPhp) generateTemplatesFiles(path string) error {
+	if err := serv.generateTemplatesFileBase(path); err != nil {
+		return err
+	}
+	if err := serv.generateTemplatesFileIndex(path); err != nil {
+		return err
+	}
+	for _, table := range *serv.tables {
+		if err := serv.generateTemplatesFile(&table, path); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// templates内のbase.html生成
+func (serv *sourceGeneratorPhp) generateTemplatesFileBase(path string) error {
+	code := fmt.Sprintf(
+		"%s\n\t{%s block content %s}{%s endblock %s}\n%s",
+		GenerateHtmlCodeHeader(serv.tables),
+		"%", "%", "%", "%",
+		GenerateHtmlCodeFooter(),
+	)
+
+	err := WriteFile(path + "base.html", code)
+	if err != nil {
+		logger.LogError(err.Error())
+	}
+	return err
+}
+
+// templates内のindex.html生成
+func (serv *sourceGeneratorPhp) generateTemplatesFileIndex(path string) error {
+	code := fmt.Sprintf(
+		"{%s extends 'base.html' %s}\n\n{%s block content %s}\n{%s endblock %s}",
+		"%", "%", "%", "%", "%", "%",
+	)
+
+	err := WriteFile(path + "index.html", code)
+	if err != nil {
+		logger.LogError(err.Error())
+	}
+	return err
+}
+
+// templates内の*.html生成
+func (serv *sourceGeneratorPhp) generateTemplatesFile(table *dto.Table, path string) error {
+	code := fmt.Sprintf(
+		"{%s extends 'base.html' %s}\n\n{%s block content %s}%s{%s endblock %s}",
+		"%", "%", "%", "%", GenerateHtmlCodeMain(table), "%", "%",
+	)
+
+	err := WriteFile(fmt.Sprintf("%s%s.html", path, table.TableName), code)
 	if err != nil {
 		logger.LogError(err.Error())
 	}
